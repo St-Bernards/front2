324f4461bf14fa96c9ce898c9d4492aeb586c957
Darius Foodeei <darius.foodei@gmail.com>
2023-02-12 03:10:05 -0500

    init front2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <image className="App-logo" src={logo} alt="logo" />
+        <header className="navbar">
+          <text className="App-title">
+            <text className="textTitle">St Bernard</text>
+          </text>
+          <button className="Button">
+            <text className="textButton">Connect Wallet</text>
+          </button>
+        </header>
       </header>
+      <body className="bodyFrame">
+        <text className="bodyText">
+          Empowering Patients with Secure and Decentralized Health Information
+          Access
+        </text>
+        <text className="bodyText2">
+          Health carely is a new way to get health insurance quotes. We offer
+          tools similar to those provided by insurance companies for free and
+          prices are based on donations and not restrictive health plan
+          networks.
+        </text>
+        <body className="buttonSet">
+          <button className="Button">
+            <text className="textButton">Doctor</text>
+          </button>
+          <button className="Button">
+            <text className="textButton">Patient</text>
+          </button>
+        </body>
+      </body>
     </div>
   );
 }
